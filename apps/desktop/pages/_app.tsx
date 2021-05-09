ea6f3ff0ac943253b9ca970196aec8bce693940c
morteza-jamali <mortezajamali4241@gmail.com>
2021-05-10 01:00:36 +0430

    feat: login page created

--- apps/desktop/pages/_app.tsx
+++ apps/desktop/pages/_app.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import { AppProps } from 'next/app';
 import { Theme as UWPThemeProvider, getTheme } from 'react-uwp/Theme';
 import './styles.css';
 
+React.useLayoutEffect = React.useEffect;
+
 const theme = getTheme({
-  themeName: 'dark',
-  accent: '#0078D7',
   useFluentDesign: true,
-  desktopBackgroundImage: 'https://picsum.photos/700/700',
 });
 
+const themeContainerStyles: CSSProperties = {
+  position: 'fixed',
+  top: 0,
+  right: 0,
+  left: 0,
+  bottom: 0,
+};
+
 const App = ({ Component, pageProps }: AppProps) => (
-  <UWPThemeProvider theme={theme}>
+  <UWPThemeProvider theme={theme} style={themeContainerStyles}>
     <Component {...pageProps} />
   </UWPThemeProvider>
 );
