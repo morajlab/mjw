386f0daf414392b4b40e8f25c8b71914913aeddb
morteza-jamali <mortezajamali4241@gmail.com>
2021-05-10 07:24:01 +0430

    chore: local react-uwp structure reseted to v1.2.2 tag

--- apps/desktop/pages/_app.tsx
+++ apps/desktop/pages/_app.tsx
@@ -1,12 +1,15 @@
 import React, { CSSProperties } from 'react';
 import { AppProps } from 'next/app';
-import { Theme as UWPThemeProvider, getTheme } from 'react-uwp/Theme';
+import { Theme as UWPThemeProvider, getTheme } from '@react-uwp/Theme';
 import './styles.css';
 
 React.useLayoutEffect = React.useEffect;
 
 const theme = getTheme({
+  themeName: 'dark',
+  accent: '#0078D7',
   useFluentDesign: true,
+  desktopBackgroundImage: 'bg-1.jpg',
 });
 
 const themeContainerStyles: CSSProperties = {
--- apps/desktop/pages/index.tsx
+++ apps/desktop/pages/index.tsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Persona, PersonaSize, Stack, IStackStyles } from '@fluentui/react';
 import { TestImages } from '@fluentui/example-data';
-import { Theme, getTheme } from 'react-uwp';
 
 export default class Desktop extends Component {
   static contextTypes = { theme: PropTypes.object };
@@ -11,41 +10,34 @@ export default class Desktop extends Component {
   render() {
     const { theme } = this.context;
 
-    const loginTheme = getTheme({
-      useFluentDesign: true,
-      desktopBackgroundImage: 'bg-1.jpg',
-    });
-
     const containerStackStyles: IStackStyles = {
       root: {
         width: '100%',
         height: '100%',
-        background: 'none',
+        background: theme.acrylicTexture80.background,
       },
     };
 
     return (
-      <Theme theme={loginTheme}>
-        <Stack styles={containerStackStyles}>
-          <Stack grow={1} verticalAlign="center" horizontalAlign="center">
-            <Stack.Item>
-              <Persona
-                imageUrl={TestImages.personaFemale}
-                imageInitials="AL"
-                size={PersonaSize.size120}
-              />
-            </Stack.Item>
-          </Stack>
-          <Stack>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
-              voluptates odio soluta veritatis provident quas ipsum aut vitae,
-              similique, amet eius quam quia corporis ut tenetur, possimus ipsam
-              asperiores vero?
-            </p>
-          </Stack>
+      <Stack styles={containerStackStyles}>
+        <Stack grow={1} verticalAlign="center" horizontalAlign="center">
+          <Stack.Item>
+            <Persona
+              imageUrl={TestImages.personaFemale}
+              imageInitials="AL"
+              size={PersonaSize.size120}
+            />
+          </Stack.Item>
         </Stack>
-      </Theme>
+        <Stack>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+            voluptates odio soluta veritatis provident quas ipsum aut vitae,
+            similique, amet eius quam quia corporis ut tenetur, possimus ipsam
+            asperiores vero?
+          </p>
+        </Stack>
+      </Stack>
     );
   }
 }
