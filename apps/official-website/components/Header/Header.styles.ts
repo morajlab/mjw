e78d2f8e1e8be37bf94369d5c0a21b05aeaec35c
Morteza Jamali <mortezajamali4241@gmail.com>
2021-06-11 10:54:34 +0430

    chore: Header component updated

--- apps/official-website/components/Header/Header.styles.ts
+++ apps/official-website/components/Header/Header.styles.ts
@@ -1,12 +1,16 @@
+import { css } from 'glamor';
 import type { IHeaderStyleProps } from './Header.types';
 
 export const Styles = ({}: IHeaderStyleProps) => {
   return {
-    root: {},
-    caption: {
-      backgroundColor: 'rgba(86,61,124,0.15)',
-      border: '1px solid rgba(86,61,124,0.15)',
-    },
+    root: css({}),
+    caption: css({
+      background:
+        'linear-gradient(90deg, rgb(90, 24, 154, 0.4) 0%, rgba(255,255,255,1) 70%, rgba(255,255,255,1) 100%)',
+    }),
+    slogan: css({
+      fontSize: '1.5rem',
+    }),
   };
 };
 
--- apps/official-website/components/Header/Header.tsx
+++ apps/official-website/components/Header/Header.tsx
@@ -1,26 +1,27 @@
 import React, { FunctionComponent } from 'react';
-import { css } from 'glamor';
 import { Container, Row, Col } from 'shards-react';
 import { Styles } from './Header.styles';
 import type { IHeaderProps } from './Header.types';
 
 export const Header: FunctionComponent<IHeaderProps> = ({}) => {
-  const { root, caption } = Styles({});
+  const { root, caption, slogan } = Styles({});
 
   return (
-    <Container className="p-5" fluid {...css(root)}>
+    <Container className="p-5" fluid {...root}>
       <Row>
-        <Col>
+        <Col className="pt-5 pl-5">
           <h1 className="display-4 text-uppercase font-weight-bolder d-flex flex-column">
             <span>Welcome to</span>
-            <span className="text-nowrap ml-5" {...css(caption)}>
+            <span className="text-nowrap ml-5" {...caption}>
               Morajlab
             </span>
             <span>official website</span>
           </h1>
-          <h3>We love open-source and work hardly on it to improve it</h3>
+          <em {...slogan}>
+            We love open-source and work hardly on it to improve it
+          </em>
         </Col>
-        <Col>Right side</Col>
+        <Col lg={4}>Right side</Col>
       </Row>
     </Container>
   );
