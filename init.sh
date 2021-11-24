e5ec4a255548f7ee4fa58496f4ab1fa64ab97503
Morteza Jamali <mortezajamali4241@gmail.com>
2021-11-24 19:55:35 +0000

    feat(scripts): create 'clone' command

--- init.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-# Clone POPO cli tool
-cd ./packages && git clone https://github.com/morajlab/popo-cli.git
---
+++ package.sh
@@ -0,0 +1,6 @@
+declare -A packages
+
+packages=(
+  ["popo-cli"]="https://github.com/morajlab/popo-cli.git"
+  ["nodexe"]="https://github.com/morajlab/nodexe.git"
+)
