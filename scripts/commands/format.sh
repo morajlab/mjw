51d3ca8d11e7467897bb80c084c2b898b151318e
Morteza Jamali <mortezajamali4241@gmail.com>
2021-12-26 02:58:53 +0000

    refactor(scripts): remove `--init` option from `format` command

--- scripts/commands/format.sh
+++ scripts/commands/format.sh
@@ -1,45 +1,31 @@
-# Options
-INIT_OPT="--init"
-
+FORMATTER_CONFIG_FILE_NAME=".prettierrc.js"
 CACHE_DIR_NAME=$(echo -n $(pwd) | md5sum | cut -d' ' -f1)
-CACHE_FORMAT_PATH="$MJW_CACHE_PATH/$CACHE_DIR_NAME/format"
+CACHE_FORMATTER_PATH="$MJW_CACHE_PATH/$CACHE_DIR_NAME/format"
+CACHE_FORMATTER_CONFIG_PATH=$CACHE_FORMATTER_PATH/$FORMATTER_CONFIG_FILE_NAME
+FORMATTER_BIN_PATH=$MJW_NODE_BIN_PATH/prettier
+DEST_FORMATTER_CONFIG_PATH="$(pwd)/$FORMATTER_CONFIG_FILE_NAME"
 
 init() {
-  local dest_format_config_path="$(pwd)/.prettierrc.js"
-  local dest_format_config_content=""
+  local dest_formatter_config_content=""
 
-  mkdir -p $CACHE_FORMAT_PATH
+  mkdir -p $CACHE_FORMATTER_PATH
 
-  if [[ -f $dest_format_config_path ]]; then
-    dest_format_config_content="...require(\"$dest_format_config_path\"),"
-    echo "INFO:: Custom formatter configuration founded at '$dest_format_config_path'"
+  if [[ -f $DEST_FORMATTER_CONFIG_PATH ]]; then
+    dest_formatter_config_content="...require(\"$DEST_FORMATTER_CONFIG_PATH\"),"
+    echo "INFO:: Custom formatter configuration founded at '$DEST_FORMATTER_CONFIG_PATH'"
   fi
 
-cat <<- EOF > $CACHE_FORMAT_PATH/.prettierrc.js
-module.exports = {...require("@morajlab/npm.config.prettier"),$dest_format_config_content};
+cat <<- EOF > $CACHE_FORMATTER_CONFIG_PATH
+module.exports = {...require("@morajlab/npm.config.prettier"),$dest_formatter_config_content};
 EOF
 
   echo "MJW formatter initialized successfully."
 }
 
 format() {
-  local args=${*:2}
-
-  while [ "$#" -gt 0 ]; do
-    case "${1^^}" in
-      "${INIT_OPT^^}")
-        init
-        exit 0
-      ;;
-      *)
-        shift
-      ;;
-    esac
-  done
-
-  if [[ ! -f $CACHE_FORMAT_PATH/.prettierrc.js ]]; then
+  if [[ ! -f $CACHE_FORMATTER_CONFIG_PATH ]]; then
     init
   fi
 
-  $MJW_NODE_BIN_PATH/prettier --config $CACHE_FORMAT_PATH/.prettierrc.js $args
+  $FORMATTER_BIN_PATH --config $CACHE_FORMATTER_CONFIG_PATH --write --ignore-unknown ${*:2}
 }
