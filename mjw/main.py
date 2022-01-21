ca769115610d777d3add9ca66f667190ec357b2a
Morteza Jamali <mortezajamali4241@gmail.com>
2022-01-21 19:17:01 +0000

    feat(base): create branch synchronizer

--- mjw/main.py
+++ mjw/main.py
@@ -1,7 +1,11 @@
 import os
+import subprocess
 import tomlkit
 from .path import getRootPath, getPackagesPath
+from .git import checkout
 from git import Repo
+
+root_repo = Repo(getRootPath())
 
 def install():
   with open(os.path.join(getRootPath(), "mjw.config.toml")) as f:
@@ -17,6 +21,24 @@ def install():
       else:
         try:
           Repo.clone_from(repo, getPackagesPath(package))
+          checkout(getPackagesPath(package), root_repo.active_branch)
+          makefile = getPackagesPath(package, "Makefile")
+
+          if os.path.exists(makefile):
+            subprocess.run([makefile, "virtualenv"], check=True)
+
           print("Package '%s' installed." % package)
         except Exception as error:
           raise error
+
+
+
+def sync():
+  with os.scandir(getPackagesPath()) as ent:
+    for entry in ent:
+        if entry.is_dir():
+            checkout(getPackagesPath(entry.name), str(root_repo.active_branch))
+            print("Checkout package '{}' to branch '{}'".format(entry.name, root_repo.active_branch))
+
+def mjw():
+  pass
