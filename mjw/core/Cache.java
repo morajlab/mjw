92958ec14db6cdc68fd2229e0d2112d98d602577
Morteza Jamali <mortezajamali4241@gmail.com>
2022-03-14 23:53:24 +0000

    chore(core): improve `Cache.java` functionality

--- mjw/core/Cache.java
+++ mjw/core/Cache.java
@@ -1,26 +1,35 @@
 import java.math.BigInteger;
 import java.io.File;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
-// import org.apache.commons.io.FileUtils;
+import java.security.NoSuchAlgorithmException;
 
 public class Cache {
   private final String CACHE_DIR_PREFIX = ".cache";
   private final String key_hash;
   private final String key;
 
-  public Cache(String key) {
+  public Cache(String key) throws Exception {
     this.key = key;
     this.key_hash = getKeyHash();
   }
 
-  // public static boolean clean() {
-  //   FileUtils.deleteDirectory(new File(destination));
-  // }
+  public void clean(File file) throws IOException {
+    if (file.isDirectory()) {
+      for (File c : file.listFiles()) {
+        clean(c);
+      }
+    }
 
-  // public static boolean clean(boolean all) {}
+    if (!file.delete()) {
+      throw new FileNotFoundException("Failed to delete file: " + file);
+    }
+  }
 
   public String getCachePath() {
-    return this.CACHE_DIR_PREFIX + File.separator + getKeyHash();
+    return this.CACHE_DIR_PREFIX + File.separator + this.key_hash;
   }
 
   public void init() {
@@ -31,25 +40,19 @@ public class Cache {
     }
   }
 
-  private String getKeyHash() {
+  private String getKeyHash() throws UnsupportedEncodingException, NoSuchAlgorithmException {
     if (this.key_hash != null && !this.key_hash.isBlank()) {
       return this.key_hash;
     }
 
-    String hash_string = "";
+    byte[] key_bytes = key.getBytes("UTF-8");
+    MessageDigest md = MessageDigest.getInstance("MD5");
+    byte[] hash_digest = md.digest(key_bytes);
+    BigInteger big_int = new BigInteger(1, hash_digest);
+    String hash_string = big_int.toString(16);
 
-    try {
-      byte[] key_bytes = key.getBytes("UTF-8");
-      MessageDigest md = MessageDigest.getInstance("MD5");
-      byte[] hash_digest = md.digest(key_bytes);
-      BigInteger big_int = new BigInteger(1, hash_digest);
-      hash_string = big_int.toString(16);
-
-      while(hash_string.length() < 32){
-        hash_string = "0" + hash_string;
-      }
-    } catch (Exception ex) {
-      // Create your exception or re-throw ex object
+    while(hash_string.length() < 32){
+      hash_string = "0" + hash_string;
     }
 
     return hash_string;
